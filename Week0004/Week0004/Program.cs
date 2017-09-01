222f7dac91eb1b22172fd593dbc0bdd11c02d9c3
Sukanya <Sukanyak21130@gmail.com>
2017-09-01 12:02:26 +0700

    Lab7

--- Week0004/Week0004/Program.cs
+++ Week0004/Week0004/Program.cs
@@ -10,8 +10,13 @@ namespace Week0004
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-           
+            const double i = 123.456789d;
+            Console.WriteLine("{0:F1}", i);
+            Console.WriteLine("{0:F2}", i);
+            Console.WriteLine("{0:F3}", i);
+            Console.WriteLine("{0:F4}", i);
+            Console.WriteLine("{0:F5}", i);
+
         }
     }
 }
