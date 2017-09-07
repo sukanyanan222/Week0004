ba095c8a902c410b3f11d848c15c56c3d86ed0ba
Sukanya <Sukanyak21130@gmail.com>
2017-09-07 14:23:38 +0700

    W04P13

--- Week0004/Week0004/Program.cs
+++ Week0004/Week0004/Program.cs
@@ -10,8 +10,15 @@ namespace Week0004
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-           
+            Console.Write("name :");
+            string val1 = Console.ReadLine();
+            Console.Write("Lastname :");
+            string val2 = Console.ReadLine();
+            Console.Write("ID :");
+            double val3  = Convert.ToInt32(Console.ReadLine());
+            Console.Write("GPA :");
+            double val4 = Convert.ToInt32(Console.ReadLine());
+
         }
     }
 }
