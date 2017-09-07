47566bfdb0e70893b6118f8375e15fa16c7bf345
Sukanya <Sukanyak21130@gmail.com>
2017-09-07 13:07:59 +0700

    W04P09

--- Week0004/Week0004/Program.cs
+++ Week0004/Week0004/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Week0004
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-           
+
+            char ch;
+            Console.Write("Press a key followed by ENTER:");
+            ch = (char)Console.Read();  // get a char
+            Console.WriteLine("Your key is: " + ch);
+
+
         }
     }
 }
